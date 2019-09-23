e0f88ac96e6cf0871df3dbd1d21112b07c0eb2f4
Anna Debruyere <fpearl@mac.com>
2019-09-23 09:17:53 -0500

    Initial Commit

---
+++ Cupcake Corner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cupcake Corner
+//
+//  Created by Anna Debruyere on 9/23/19.
+//  Copyright © 2019 Eclipse Scriptworks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
